9ff084b29575afd5e805409314237ffde9ffc4d4
Vladimir <todrinktea4@gmail.com>
2021-08-01 18:26:18 +0300

    добавил модуль для списка точек маршрута; отрисовал элементы на странице согласно заданию

--- src/main.js
+++ src/main.js
@@ -1,3 +1,34 @@
+import {createMenuTemplate} from './view/menu.js'
+import {createSortTemplate} from './view/sort.js'
+import {createInfoTemplate} from './view/info.js'
+import {createFiltersTemplate} from './view/filters.js'
+import {createPointTemplate} from './view/point.js'
+import {createAddPointTemplate} from './view/add-point.js'
+import {createEditPointTemplate} from './view/edit-point.js'
+import {createEventsListTemplate} from './view/events-list.js'
+
+const POINT_COUNT = 3;
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
+
+const siteMainElement = document.querySelector('.trip-main');
+const siteTripControlsNavElement = document.querySelector('.trip-controls__navigation');
+const siteTripControlsFilterElement = document.querySelector('.trip-controls__filters');
+const siteTripEventsElement = document.querySelector('.trip-events');
+
+render(siteMainElement, createInfoTemplate(), 'afterbegin');
+render(siteTripControlsNavElement, createMenuTemplate(), 'beforeend');
+render(siteTripControlsFilterElement, createFiltersTemplate(), 'beforeend');
+render(siteTripEventsElement, createSortTemplate(), 'beforeend');
+render(siteTripEventsElement, createEventsListTemplate(), 'beforeend');
+
+const siteTripEventsListElement = document.querySelector('.trip-events__list');
+
+render(siteTripEventsListElement, createEditPointTemplate(), 'beforeend');
+
+for (let i = 0; i < POINT_COUNT; i++) {
+  render(siteTripEventsListElement, createPointTemplate(), 'beforeend');
+}
+
