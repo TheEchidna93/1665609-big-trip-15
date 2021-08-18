7d7d1618833942a37eb4c6faccd33ed2644dd4b0
Vladimir <todrinktea4@gmail.com>
2021-08-18 21:49:49 +0300

    переделал функцию рендеринга; переделал фильтры как класс

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import {createPointTemplate} from './view/point.js';
 import {createEditPointTemplate} from './view/edit-point.js';
 import {createEventsListTemplate} from './view/events-list.js';
 import {getPoint} from './mock/point.js';
+import {renderTemplate} from './utils.js';
 
 const POINT_COUNT = 20;
 let interval = 0;
@@ -20,26 +21,22 @@ for (let i = 0; i < POINT_COUNT; i++) {
   hoursPassed += interval;
 }
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const siteMainElement = document.querySelector('.trip-main');
 const siteTripControlsNavElement = document.querySelector('.trip-controls__navigation');
 const siteTripControlsFilterElement = document.querySelector('.trip-controls__filters');
 const siteTripEventsElement = document.querySelector('.trip-events');
 
-render(siteMainElement, createInfoTemplate(points), 'afterbegin');
-render(siteTripControlsNavElement, createMenuTemplate(), 'beforeend');
-render(siteTripControlsFilterElement, createFiltersTemplate(), 'beforeend');
-render(siteTripEventsElement, createSortTemplate(), 'beforeend');
-render(siteTripEventsElement, createEventsListTemplate(), 'beforeend');
+renderTemplate(siteMainElement, createInfoTemplate(points), 'afterbegin');
+renderTemplate(siteTripControlsNavElement, createMenuTemplate(), 'beforeend');
+renderTemplate(siteTripControlsFilterElement, createFiltersTemplate(), 'beforeend');
+renderTemplate(siteTripEventsElement, createSortTemplate(), 'beforeend');
+renderTemplate(siteTripEventsElement, createEventsListTemplate(), 'beforeend');
 
 const siteTripEventsListElement = document.querySelector('.trip-events__list');
 
-render(siteTripEventsListElement, createEditPointTemplate(points[0]), 'beforeend');
+renderTemplate(siteTripEventsListElement, createEditPointTemplate(points[0]), 'beforeend');
 
 for (let i = 1; i < POINT_COUNT; i++) {
-  render(siteTripEventsListElement, createPointTemplate(points[i]), 'beforeend');
+  renderTemplate(siteTripEventsListElement, createPointTemplate(points[i]), 'beforeend');
 }
 
