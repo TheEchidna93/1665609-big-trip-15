1b8e8f39fc6762a5fe3c35b074cbbca9d4ef6f9c
Vladimir <todrinktea4@gmail.com>
2021-08-24 20:47:57 +0300

    прогнал через линтер

--- src/view/no-point.js
+++ src/view/no-point.js
@@ -6,7 +6,7 @@ export default class NoPoint {
   }
 
   getTemplate() {
-    return `<p class="trip-events__msg">Click New Event to create your first point</p>`;
+    return '<p class="trip-events__msg">Click New Event to create your first point</p>';
   }
 
   getElement() {
